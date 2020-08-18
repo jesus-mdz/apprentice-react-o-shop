bd158228ff48f54571411d20b78b757eac8c0cf6
Jesus Mendoza <jesusmendoza@Jesuss-MBP.attlocal.net>
2020-08-18 09:50:12 -0500

    feat(o-shop): remove logs

--- src/services/userService.js
+++ src/services/userService.js
@@ -5,7 +5,6 @@ export const userService = {
 };
 
 function login(object) {
-  console.log(object);
   const requestOptions = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
